5e056ff5d5ff77cb8ede390ab43b84f20891508f
karthikdhanusu <36257249+karthikdhanusu@users.noreply.github.com>
2026-01-22 20:43:27 +0530

    bastionoci.py

--- bastionoci.py
+++ bastionoci.py
@@ -28,20 +28,26 @@ sessiondetails = oci.bastion.models.CreateSessionDetails( \
     session_ttl_in_seconds = 10700)
 
 
+
+
+
 for i in listsession:
     if i.lifecycle_state == 'ACTIVE' and session_suffix in i.display_name:
-        deletesession = basti.delete_session(i.id).data
-        print("Old session is deleted")
+        getsession = basti.get_session(i.id).data
+        ssh_cmd = "ssh -i " + privkeypath + " -N -D 127.0.0.1:1080 -p 22 " + i.id + "@host.bastion." + \
+                  ociconfig['region'] + '.oci.oraclecloud.com'
+        print("Session is Active")
+        ssh_process = subprocess.run(ssh_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-while True:
-    print("Creating a New Bastion Session")
-    createsesion = basti.create_session(sessiondetails).data
-    while createsesion.lifecycle_state != 'ACTIVE':
-        getseson = basti.get_session(createsesion.id).data
-        createsesion.lifecycle_state = getseson.lifecycle_state
-        time.sleep(20)
-    print("Bastion session is Active")
-    ssh_cmd = "ssh -i " + privkeypath + " -N -D 127.0.0.1:1080 -p 22 "+ createsesion.id+"@host.bastion."+ociconfig['region']+'.oci.oraclecloud.com'
-    ssh_process = subprocess.run(ssh_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, timeout=10500)
-    deletesession = basti.delete_session(createsesion.id).data
-    print("Bastion session is deleted")
+    else :
+        print("Creating a New Bastion Session")
+        createsesion = basti.create_session(sessiondetails).data
+        while createsesion.lifecycle_state != 'ACTIVE':
+            getseson = basti.get_session(createsesion.id).data
+            createsesion.lifecycle_state = getseson.lifecycle_state
+            time.sleep(20)
+        print("Bastion session is Active")
+        ssh_cmd = "ssh -i " + privkeypath + " -N -D 127.0.0.1:1080 -p 22 "+ createsesion.id+"@host.bastion."+ociconfig['region']+'.oci.oraclecloud.com'
+        ssh_process = subprocess.run(ssh_cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        deletesession = basti.delete_session(createsesion.id).data
+        print("Bastion session is deleted")
